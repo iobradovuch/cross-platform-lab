0f91f27cf743ea6e0238855e8397e88d458fb2a7
iobradovuch <iobradovuch@gmail.com>
2024-03-15 17:28:19 +0200

    Initial commit

---
+++ src/main/java/ua/iobradovuch/lab/controllers/DemoController.java
@@ -0,0 +1,20 @@
+package ua.iobradovuch.lab.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/")
+public class DemoController {
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("pageTitle", "Hello Spring!");
+        model.addAttribute("languages", List.of("Java", "Kotlin", "Groovy"));
+        return "index";
+    }
+}
